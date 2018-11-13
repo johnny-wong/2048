b89b7695ee00f4e4ba873914ff749634fdc6cf9f
Johnny Wong <hin.wong2@yahoo.com.au>
2018-11-13 18:24:21 +1100

    convert to unittest module format

--- test_game2048.py
+++ test_game2048.py
@@ -1,53 +1,51 @@
 import game2048
+import unittest
 
-def test_combine():
-	game_1 = game2048.Game()
+class TestSwipes(unittest.TestCase):
 
-	# no change
-	assert(game_1._combine([0,0,0,0]) == ([0,0,0,0], 0))
-	assert(game_1._combine([2,0,0,0]) == ([2,0,0,0], 0))
-	assert(game_1._combine([2,4,8,16]) == ([2,4,8,16], 0))
-	assert(game_1._combine([2,4,0,0]) == ([2,4,0,0], 0))
-	assert(game_1._combine([2,4,8,16,32])) == ([2, 4, 8, 16, 32], 0)
-	
-	# one combine
-	assert(game_1._combine([2,2,0,0]) == ([4,0,0,0], 4))
-	assert(game_1._combine([2,2,2,0]) == ([4,2,0,0], 4))
-	assert(game_1._combine([4,4,2,8]) == ([8,2,8,0], 8))
-	assert(game_1._combine([2,4,4,8]) == ([2,8,8,0], 8))
-	assert(game_1._combine([2,4,8,8]) == ([2,4,16,0], 16))
-	assert(game_1._combine([2,4,8,8, 2]) == ([2,4,16,2,0], 16))
-	
-	# two combine
-	assert(game_1._combine([2,2,2,2]) == ([4,4,0,0], 8))
-	assert(game_1._combine([2,2,4,4]) == ([4,8,0,0], 12))
-	assert(game_1._combine([8,8,4,4]) == ([16,8,0,0], 24))
-	assert(game_1._combine([1,8,8,4,4,1]) == ([1,16,8,1,0, 0], 24))
+	def test_combine(self):
+		game_1 = game2048.Game()
 
-	# Three combine
-	assert(game_1._combine([1,8,8,1,4,4,2,2,4]) == ([1,16,1,8,4,4,0,0,0], 28))
+		# no change
+		self.assertEqual(game_1._combine([0,0,0,0]), ([0,0,0,0], 0))
+		self.assertEqual(game_1._combine([2,0,0,0]), ([2,0,0,0], 0))
+		self.assertEqual(game_1._combine([2,4,8,16]), ([2,4,8,16], 0))
+		self.assertEqual(game_1._combine([2,4,0,0]), ([2,4,0,0], 0))
+		self.assertEqual(game_1._combine([2,4,8,16,32]), ([2, 4, 8, 16, 32], 0))
+		
+		# one combine
+		self.assertEqual(game_1._combine([2,2,0,0]), ([4,0,0,0], 4))
+		self.assertEqual(game_1._combine([2,2,2,0]), ([4,2,0,0], 4))
+		self.assertEqual(game_1._combine([4,4,2,8]), ([8,2,8,0], 8))
+		self.assertEqual(game_1._combine([2,4,4,8]), ([2,8,8,0], 8))
+		self.assertEqual(game_1._combine([2,4,8,8]), ([2,4,16,0], 16))
+		self.assertEqual(game_1._combine([2,4,8,8, 2]), ([2,4,16,2,0], 16))
+		
+		# two combine
+		self.assertEqual(game_1._combine([2,2,2,2]), ([4,4,0,0], 8))
+		self.assertEqual(game_1._combine([2,2,4,4]), ([4,8,0,0], 12))
+		self.assertEqual(game_1._combine([8,8,4,4]), ([16,8,0,0], 24))
+		self.assertEqual(game_1._combine([1,8,8,4,4,1]), ([1,16,8,1,0, 0], 24))
 
-	print('Combine tests passed!')
+		# Three combine
+		self.assertEqual(game_1._combine([1,8,8,1,4,4,2,2,4]), ([1,16,1,8,4,4,0,0,0], 28))
 
-def test_shift():
-	game_1 = game2048.Game()
-	# no combine
-	assert(game_1._shift([0, 0, 0, 1, 2, 4]) == ([1, 2, 4, 0, 0, 0], 0))
-	assert(game_1._shift([1, 2, 4, 0]) == ([1, 2, 4, 0], 0))
-	
-	# one combine
-	assert(game_1._shift([1, 2, 2, 0]) == ([1, 4, 0, 0], 4))
-	assert(game_1._shift([1, 0, 2, 2]) == ([1, 4, 0, 0], 4))
-	assert(game_1._shift([0, 1, 0, 1]) == ([2, 0, 0, 0], 2))
-	
-	# two combine
-	assert(game_1._shift([0, 1, 0, 1, 2, 0, 2]) == ([2, 4, 0, 0, 0, 0, 0], 6))
-	
-	# three combine
-	assert(game_1._shift([1, 1, 1, 1, 1, 1]) == ([2, 2, 2, 0, 0, 0], 6))
-	
-	print('Shift tests passed!')
-	
-test_shift()
-test_combine()
-	
+	def test_shift(self):
+		game_1 = game2048.Game()
+		# no combine
+		self.assertEqual(game_1._shift([0, 0, 0, 1, 2, 4]), ([1, 2, 4, 0, 0, 0], 0))
+		self.assertEqual(game_1._shift([1, 2, 4, 0]), ([1, 2, 4, 0], 0))
+		
+		# one combine
+		self.assertEqual(game_1._shift([1, 2, 2, 0]), ([1, 4, 0, 0], 4))
+		self.assertEqual(game_1._shift([1, 0, 2, 2]), ([1, 4, 0, 0], 4))
+		self.assertEqual(game_1._shift([0, 1, 0, 1]), ([2, 0, 0, 0], 2))
+		
+		# two combine
+		self.assertEqual(game_1._shift([0, 1, 0, 1, 2, 0, 2]), ([2, 4, 0, 0, 0, 0, 0], 6))
+		
+		# three combine
+		self.assertEqual(game_1._shift([1, 1, 1, 1, 1, 1]), ([2, 2, 2, 0, 0, 0], 6))
+
+if __name__ == '__main__':
+    unittest.main()
