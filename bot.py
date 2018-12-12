37e4865e3d2fdcaaf210ca2d55cc4c189beef26b
Johnny Wong <hin.wong2@yahoo.com.au>
2018-12-12 22:50:09 +1100

    Create random bot

--- bot.py
+++ bot.py
@@ -1,6 +1,29 @@
 import game2048
+import random
 
-class bot:
+class BotPlayer:
+	def __init__(self, game, bot):
+		if not isinstance(game, game2048.Game):
+			raise TypeError('game must be of type Game defined in game2048.py')
+
+		if 'decide_move' not in dir(bot):
+			raise ValueError('bot must have "decide_move" method')
+
+		self.game = game
+		self.bot = bot
+
+	def play(self):
+		''' 
+		Runs bot over game until game over. Returns the game
+		'''
+		while self.game.get_playing():
+			move = self.bot.decide_move()
+			self.game.swipe(move)
+
+	def get_game(self):
+		return self.game
+
+class RandomBot:
 	def __init__(self, game):
 		if not isinstance(game, game2048.Game):
 			raise TypeError('game must be of type Game defined in game2048.py')
@@ -8,9 +31,19 @@ class bot:
 		self.game = game
 	
 	def decide_move(self):
-		pass
+		''' Returns up, down, left, right '''
+		valid_moves = self.game.get_valid_moves()
+		if len(valid_moves) == 0:
+			return 'quit'
+
+		choice = random.choice(valid_moves)
+
+		return choice
 
 
 game_1 = game2048.Game()
-bot_1 = bot(game_1)
+random_bot = RandomBot(game_1)
 
+bot_player = BotPlayer(game_1, random_bot)
+bot_player.play()
+print(bot_player.get_game())
